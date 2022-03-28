6089ae78439bea11f3a0d8152f5ce30c25987c9c
ykiZheng <3190102363@zju.edu.cn>
2022-03-29 01:19:35 +0800

    :tada: Initialize

---
+++ src/main/java/com/segroup/hospitalsite/HospitalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.segroup.hospitalsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/segroup/hospitalsite/HospitalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segroup.hospitalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
