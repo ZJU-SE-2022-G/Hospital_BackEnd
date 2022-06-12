b3d943c722bd050b09d771782fec9bb9d4952026
ZJU3190105746 <68105073+ZJU3190105746@users.noreply.github.com>
2022-06-12 19:46:12 +0800

    Add files via upload

---
+++ src/NGP/mapper/GuideMapper.java
@@ -0,0 +1,18 @@
+package com.segroup.hospitalsite.NGP.mapper;
+
+import com.segroup.hospitalsite.NGP.entity.Guide;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Liu Yibin
+ * @since 2022-5-26
+ */
+@Mapper
+public interface GuideMapper extends BaseMapper<Guide> {
+
+}
